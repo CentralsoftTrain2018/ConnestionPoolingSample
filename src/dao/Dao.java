a2529a53d178d22a44354d0b1e9a051b91b8a2ed
eiichi hayashi <eiichi@mac.com>
2018-06-07 15:42:31 +0900

    Update Dao.java

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -21,7 +21,7 @@ public abstract class Dao
             throws SQLException
     {
         InitialContext context;
-        DataSource ds =null;
+        DataSource ds =1;
         try
         {
             context = new InitialContext();
