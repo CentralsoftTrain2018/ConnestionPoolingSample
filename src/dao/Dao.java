ffb674e711cf0adda76af7f752ff0f7f1299aa79
eiichi hayashi <eiichi@mac.com>
2018-06-07 14:45:43 +0900

    Update Dao.java

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.SQLException;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
